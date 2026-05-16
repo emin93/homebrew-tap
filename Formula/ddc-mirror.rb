7574810fca0f5f8a2761deeebb138c6f2a8b8ccd
Emin Khateeb <emin@emin.ch>
2026-05-16 13:36:56 -0500

    Update ddc-mirror to v0.1.3

--- Formula/ddc-mirror.rb
+++ Formula/ddc-mirror.rb
@@ -3,10 +3,10 @@
 
 # Formula for ddc-mirror.
 class DdcMirror < Formula
-  desc "Sync built-in MacBook brightness to all external displays"
+  desc "Smartly sync built-in MacBook brightness to external displays"
   homepage "https://ddc-mirror.emin.ch"
-  url "https://github.com/emin93/ddc-mirror/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "1c79a75e25542ef4f0c15b1d87f2d07addbae3d33ab2e3dc0e3daabbc741ea64"
+  url "https://github.com/emin93/ddc-mirror/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "46f66d28bdd933cfe1abdb7e89a0001049439776f8ec8f06adcb706bd1b49acf"
   license "MIT"
   head "https://github.com/emin93/ddc-mirror.git", branch: "main"
 
